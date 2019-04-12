f247ad3adb516c6b442d9307966b14fa9752a69b
Valeriana <valeria.do@gmail.com>
2019-04-11 23:22:50 -0500

    navigate between named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
-import 'second_route.dart';
+//import 'second_route.dart';
 
-void main() {
+/*void main() {
    runApp(MaterialApp(
      title: 'Navigation Basics',
      home: FirstRoute(),
    ));
    }
+*/
+
+void main() {
+  runApp(MaterialApp(
+title: 'Named Routes Demo',
+initialRoute: '/',
+routes: {
+  '/': (context) => FirstRoute(),
+  '/second': (context) => SecondRoute(),
+},
+  ));
+}
 
    class FirstRoute extends StatelessWidget {
      @override
@@ -19,14 +31,33 @@ void main() {
            child: RaisedButton(
              child: Text('Second Route this way!'),
              onPressed: () {
-               Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),
-               );
+               //Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),);
+               Navigator.pushNamed(context, '/second');
              },
            ),
          ),
          );
      }
    }
+
+   class SecondRoute extends StatelessWidget { //git push -u developer [in commadLine -to push to GitHub.]
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('Second Route'),
+    ),
+    body: Center(
+      child: RaisedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text('Go Back to route 1'),
+      )
+    ),
+    );
+  }
+}
 
 
 
--- lib/second_route.dart
+++ lib/second_route.dart
@@ -1,20 +1,3 @@
 import 'package:flutter/material.dart';
 
-class SecondRoute extends StatelessWidget { //working fine :)
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: AppBar(
-      title: Text('Second Route'),
-    ),
-    body: Center(
-      child: RaisedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text('Go Back to route 1'),
-      )
-    ),
-    );
-  }
-}
+

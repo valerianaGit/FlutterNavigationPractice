ebe7dea6e0b3b0fdd170e8d30a0310568f673fdf
Valeriana <valeria.do@gmail.com>
2019-04-15 12:42:39 -0500

    Organize all examples into their own files and have their main functions in main.dart for ease of access  of demos

--- lib/main.dart
+++ lib/main.dart
@@ -1,283 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
-//import 'second_route.dart';
+import 'hero_animations.dart';
+import 'navigate_to_new_screen_and_back.dart';
+import 'navigate_with_names_routes.dart';
+import 'pass_arguments_to_a_named_route.dart';
+import 'return_data_from_a_screen.dart';
+import 'send_data_to_a_new_screen.dart';
 
-/*void main() {
-   runApp(MaterialApp(
-     title: 'Navigation Basics',
-     home: FirstRoute(),
-   ));
-   }
-*/
+//hero animations
+void main() => runApp(HeroApp());
 
-/*void main() {
-  runApp(MaterialApp(
-    title: 'Named Routes Demo',
-    initialRoute: '/',
-    routes: {
-      '/': (context) => FirstRoute(),
-      '/second': (context) => SecondRoute(),
-      ExtractArgumentsScreen.routeName: (context) => ExtractArgumentsScreen(),
-    },
-  ));
-} 
+//Navigate to a new screen and back
+// void main() {
+//   runApp(MaterialApp(
+//     title: 'Navigation Basics',
+//     home: FirstRoute(),
+//   ));
+// }
 
-class FirstRoute extends StatelessWidget { 
-  final String title;                                 
-    final String message;                              
-    ScreenArguments(this.title,
-        this.message);                                 
-  @override
-  Widget build(BuildContext context) {
-                                      
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First Route - VC'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Second Route this way!'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/second');
-          },
-        ),
-      ),
-    );
-  }
-}
+//navigate with named routes
+// void main() {
+//   runApp(MaterialApp(
+//     title: 'Named Routes Demo',
+//     // Start the app with the "/" named route. In our case, the app will start
+//     // on the FirstScreen Widget
+//     initialRoute: '/',
+//     routes: {
+//       // When we navigate to the "/" route, build the FirstScreen Widget
+//       '/': (context) => FirstScreen(),
+//       // When we navigate to the "/second" route, build the SecondScreen Widget
+//       '/second': (context) => SecondScreen(),
+//     },
+//   ));
+// }
 
-class ExtractArgumentsScreen extends StatelessWidget {     
-  static const routeName = '/extractArguments';
-  @override
-  Widget build(BuildContext context) {
-    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(args.title),
-      ),
-      body: Center(
-        child: Text(args.message),
-      ),
-    );
-  }
-}
+//pass arguments to a named route
+//void main() => runApp(MyApp());
 
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Route'),
-      ),
-      body: Center(
-          child: RaisedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text('Go Back to route 1'),
-      )),
-    );
-  }
-}
-*/
-/*
-void main() => runApp(MyApp());
+//Return data from a screen 
+// void main() {
+//   runApp(MaterialApp(
+//     title: 'Returning Data',
+//     home: HomeScreen(),
+//   ));
+// }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: (settings) {
-        if (settings.name == PassArgumentsScreen.routeName) {
-          final ScreenArguments args = settings.arguments;
-          return MaterialPageRoute(
-            builder: (context) {
-              return PassArgumentsScreen(
-                title: args.title,
-                message: args.message,
-              );
-            },
-          );
-        }
-      },
-      title: 'Navigation with Arguments',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {       
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Returning Data Demo'),
-      ),
-      body: Center(
-        child: SelectionButton()),
-      );
-    
-  }
-}
-
-
-class ExtractArgumentsScreen extends StatelessWidget {
-  static const routeName = '/extractArguments';
-  @override
-  Widget build(BuildContext context) {
-    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(args.title),
-      ),
-      body: Center(
-        child: Text(args.message),
-      ),
-    );
-  }
-}
-
-
-class PassArgumentsScreen extends StatelessWidget {
-  static const routeName = '/passArguments';
-  final String title;
-  final String message;
-  const PassArgumentsScreen({
-    Key key,
-    @required this.title,
-    @required this.message,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(message),
-      ),
-    );
-  }
-}
-class ScreenArguments {               
-  final String title;
-  final String message;
-
-  ScreenArguments(this.title, this.message);
-}
-
-class SelectionButton extends StatelessWidget {  
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: Text('Pick an option, any option'),
-    );
-  }
-  _navigateAndDisplaySelection(BuildContext context) async {
-    final result = await Navigator.push(context, MaterialPageRoute(builder: (context) => SelectionScreen()),);
-    Scaffold.of(context)                                     
-    ..removeCurrentSnackBar()      
-    ..showSnackBar(SnackBar(content: Text('$result')));
-  }
-}
-                                                         
-class SelectionScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pick an Option'),
-      ),
-      body:  Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                onPressed: () {
-                 Navigator.pop(context, 'Yep!');          
-                },
-                child: Text('Yep!'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                onPressed: () {
-                 Navigator.pop(context, 'Nope');
-                },
-                child: Text('Nope'),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
-
-
-class Todo {                                   //1 . - Define a Model for Todo, has title and description
-  final String title;
-  final String description;
-  Todo(this.title, this.description);
-}
-
-void main() {                                 // generate the list view in the main function 
-  runApp(MaterialApp(
-    title: 'Passing Data',
-    home: TodosScreen(
-      todos: List.generate(
-        20, (i) => Todo(
-          'Todo $i',
-          'A description of what needs to be done for Todo $i',
-        ),),
-    ),
-    ));
-}
-
-class TodosScreen extends StatelessWidget {  
-  final List<Todo> todos;
-  TodosScreen({Key key, @required this.todos}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: Text('Todos'),
-     ),
-     body: ListView.builder(  
-      itemCount: todos.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(todos[index].title),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(todo: todos[index]),),);
-          },
-        );
-      },
-    ),
-   );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  final Todo todo;
-  DetailScreen({Key key, @required this.todo}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(todo.title),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Text(todo.description),
-      ),
-    );
-  }
-}
+//send data to a new screen
+// void main() {
+//   runApp(MaterialApp(
+//     title: 'Passing Data',
+//     home: TodosScreen(
+//       todos: List.generate(
+//         20,
+//         (i) => Todo(
+//               'Todo $i',
+//               'A description of what needs to be done for Todo $i',
+//             ),
+//       ),
+//     ),
+//   ));
+// }
